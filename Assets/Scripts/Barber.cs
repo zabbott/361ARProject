1adc99b9682272ef10d8927a0e7d0bb4303486af
NeonIcarus <ctk50101@gmail.com>
2022-05-03 19:51:46 -0500

    Drag Queen

--- Assets/Scripts/Barber.cs
+++ Assets/Scripts/Barber.cs
@@ -25,5 +25,27 @@ public class Barber : MonoBehaviour
 
     }
 
+    public GameObject StacheHolder;
+    private float height;
+    private Vector2 StacheHeight;
+
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            Debug.Log("Touch");
+
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector2 pos = touch.position;
+                pos.y = (pos.y - height) / height;
+                StacheHeight = new Vector2(StacheHolder.transform.position.x, pos.y);
+
+                StacheHolder.transform.position = StacheHeight;
+            }
+        }
+    }
 
 }
